74c97c7064d045b6a9529903c7e9a6e222ac8d19
Salvatore Poliandro III <sal@opsitters.com>
2015-09-23 13:14:46 -0400

    forgot to pull this when pulling chef

--- lib/opstack/environment.rb
+++ lib/opstack/environment.rb
@@ -1,6 +1,3 @@
-require 'chef/encrypted_data_bag_item'
-require 'chef/encrypted_data_bag_item/decryption_failure'
-require 'chef/encrypted_data_bag_item/encryption_failure'
 require 'json'
 require 'yaml'
 
